e19f0cdb0b15702e30ed1d4ac6fe71ba77ac3dfc
sujitha-sekar <snowashii2002@gmail.com>
2024-06-11 15:55:41 +0530

    project setup

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,102 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule, MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatDialogModule } from '@angular/material/dialog';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatTooltipModule} from '@angular/material/tooltip';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    FlexLayoutModule,
+    RouterModule,
+    MatFormFieldModule,
+    MatSlideToggleModule,
+    MatInputModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatTableModule,
+    MatSortModule,
+    MatButtonToggleModule,
+    MatMenuModule,
+    MatCardModule,
+    MatPaginatorModule,
+    MatChipsModule,
+    MatDividerModule,
+    FormsModule,
+    MatDialogModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    ReactiveFormsModule,
+    MatSnackBarModule,
+    MatSliderModule,
+    MatTooltipModule
+  ], exports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    FlexLayoutModule,
+    RouterModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatTableModule,
+    MatSortModule,
+    MatButtonToggleModule,
+    MatSlideToggleModule,
+    MatMenuModule,
+    MatCardModule,
+    MatPaginatorModule,
+    MatChipsModule,
+    MatDividerModule,
+    FormsModule,
+    MatDialogModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    ReactiveFormsModule,
+    MatSnackBarModule,
+    MatSliderModule,
+    MatTooltipModule
+  ]
+})
+export class MaterialModule { }
