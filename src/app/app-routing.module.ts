31b492c82304d0097a0cb143f84a4a6d3d19856e
sujitha-sekar <snowashii2002@gmail.com>
2024-06-11 18:59:45 +0530

    dashboard designing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './core/components/home/home.component';
+import { DashboardComponent } from './core/components/dashboard/dashboard.component';
+import { ListsComponent } from './core/components/lists/lists.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full'},
-  { path: 'home', component: HomeComponent}
+  { path: '', redirectTo: '/home/dashboard', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent, children: [
+    { path: 'dashboard', component: DashboardComponent},
+    { path: 'lists', component: ListsComponent}
+  ]}
 ];
 
 @NgModule({
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material/material.module';
 import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ListsComponent } from './components/lists/lists.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    DashboardComponent,
+    ListsComponent
   ],
   imports: [
     CommonModule,
